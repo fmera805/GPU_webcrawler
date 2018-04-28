f1848e5a7832d52e0db3ab4d6dac517f60263eaf
FM7 <fm7@FM7s-MBP.attlocal.net>
2018-04-28 14:15:19 -0700

    added logbook

--- GPU_webcrawler.py
+++ GPU_webcrawler.py
@@ -1,18 +1,25 @@
 import requests
 import os
-
 from bs4 import BeautifulSoup
-
-
 from Tkinter import *
 
+def log_writer(x):
+    with open('Logbook.txt','a') as log_file:
+        log_file.write('\n')
+        log_file.write(x)
+        log_file.write('\n')
+        log_file.close()
 
 def nvidia1080TI():
     page=1
+    log_writer(80 * '*')
+    log_writer("1080TI Log")
+    log_writer(80 * '*')
     print("Scanning for 1080 TI")
+
     while page <= 1:
         url = 'https://www.ebay.com/sch/i.html?_sop=1&_from=R40&_sacat=0&_nkw=1080+ti+gtx&_pgn='+ str(page)
-        
+
         source_code=requests.get(url)
         plain_text = source_code.text
         soup = BeautifulSoup(plain_text, "html.parser")
@@ -21,6 +28,8 @@ def nvidia1080TI():
             title = link.string
             print('')
             print(150 * '-')
+            log_writer(80 * '-')
+            log_writer(href)
             print('')
             get_Item_Title(href)
             print(href)
@@ -29,14 +38,18 @@ def nvidia1080TI():
             get_Time_Left(href)
             get_single_item_data(href)
             get_Item_Condition(href)
+
         page +=1
 
 def nvidia1080():
     page=1
+    log_writer(80 * '*')
+    log_writer("1080 Log")
+    log_writer(80 * '*')
     print("Scanning for 1080")
     while page <= 1:
         url = 'https://www.ebay.com/sch/i.html?_from=R40&_sacat=0&_sop=1&_nkw=1080+gtx&_pgn='+ str(page)
-        
+
         source_code=requests.get(url)
         plain_text = source_code.text
         soup = BeautifulSoup(plain_text)
@@ -45,6 +58,8 @@ def nvidia1080():
             title = link.string
             print('')
             print(150 * '-')
+            log_writer(80 * '-')
+            log_writer(href)
             print('')
             get_Item_Title(href)
             print(href)
@@ -58,10 +73,13 @@ def nvidia1080():
 
 def nvidia1070TI():
     page=1
+    log_writer(80 * '*')
+    log_writer("1070TI Log")
+    log_writer(80 * '*')
     print("Scanning for 1070 TI")
     while page <= 1:
         url = 'https://www.ebay.com/sch/i.html?_from=R40&_sacat=0&_sop=1&_nkw=1070+ti&_pgn='+ str(page)
-        
+
         source_code=requests.get(url)
         plain_text = source_code.text
         soup = BeautifulSoup(plain_text)
@@ -70,6 +88,8 @@ def nvidia1070TI():
             title = link.string
             print('')
             print(150 * '-')
+            log_writer(80 * '-')
+            log_writer(href)
             print('')
             get_Item_Title(href)
             print(href)
@@ -82,10 +102,13 @@ def nvidia1070TI():
 
 def nvidia1070():
     page = 1
+    log_writer(80 * '*')
+    log_writer("1070 Log")
+    log_writer(80 * '*')
     print("Scanning for 1070")
     while page <= 1:
         url = 'https://www.ebay.com/sch/i.html?_sop=1&_from=R40&_sacat=0&_nkw=1070&_pgn=' + str(page)
-        
+
         source_code = requests.get(url)
         plain_text = source_code.text
         soup = BeautifulSoup(plain_text)
@@ -94,6 +117,8 @@ def nvidia1070():
             title = link.string
             print('')
             print(150 * '-')
+            log_writer(80 * '-')
+            log_writer(href)
             print('')
             get_Item_Title(href)
             print(href)
@@ -106,10 +131,13 @@ def nvidia1070():
 
 def nvidia1060_6GB():
     page = 1
+    log_writer(80 * '*')
+    log_writer("1060 6GB Log")
+    log_writer(80 * '*')
     print("Scanning for 1060 6GB")
     while page <= 1:
         url = '1060 6gb https://www.ebay.com/sch/i.html?_sop=1&_from=R40&_sacat=0&_nkw=1060+6gb&_pgn=' + str(page)
-        
+
         source_code = requests.get(url)
         plain_text = source_code.text
         soup = BeautifulSoup(plain_text)
@@ -118,6 +146,8 @@ def nvidia1060_6GB():
             title = link.string
             print('')
             print(150 * '-')
+            log_writer(80 * '-')
+            log_writer(href)
             print('')
             get_Item_Title(href)
             print(href)
@@ -130,10 +160,13 @@ def nvidia1060_6GB():
 
 def amdRX580():
     page = 1
+    log_writer(80 * '*')
+    log_writer("RX580 Log")
+    log_writer(80 * '*')
     print("Scanning for RX580")
     while page <= 1:
         url = 'https://www.ebay.com/sch/i.html?_from=R40&_sacat=0&_sop=1&_nkw=rx+580&_pgn=' + str(page)
-        
+
         source_code = requests.get(url)
         plain_text = source_code.text
         soup = BeautifulSoup(plain_text)
@@ -142,6 +175,8 @@ def amdRX580():
             title = link.string
             print('')
             print(150 * '-')
+            log_writer(80 * '-')
+            log_writer(href)
             print('')
             get_Item_Title(href)
             print(href)
@@ -151,6 +186,7 @@ def amdRX580():
             get_single_item_data(href)
             get_Item_Condition(href)
         page += 1
+
 
 def get_Item_Title(Item_Title):
     source_code = requests.get(Item_Title)
@@ -163,24 +199,29 @@ def get_Item_Price(Item_Price):
     source_code = requests.get(Item_Price)
     plain_text = source_code.text
     soup = BeautifulSoup(plain_text)
+
     for Item_Price in soup.findAll('span', {'id': 'prcIsum'}):
         print('Buy It Now:')
         print(Item_Price.string)
-
+        log_writer('Buy it now:')
+        log_writer(str(Item_Price))
 
 def get_BidPrice(item_BidPrice):
     source_code = requests.get(item_BidPrice)
     plain_text = source_code.text
     soup = BeautifulSoup(plain_text)
+
     for item_BidPrice in soup.findAll('span', {'id': 'prcIsum_bidPrice'}):
         print('Current Bid:')
         print(item_BidPrice.string)
-
+        log_writer('Current bid:')
+        log_writer(str(item_BidPrice))
 
 def get_single_item_data(item_location):
     source_code = requests.get(item_location)
     plain_text = source_code.text
     soup = BeautifulSoup(plain_text)
+
     for item_location in soup.findAll('span', {'itemprop': 'availableAtOrFrom'}):
         print('Item Location:')
         print(item_location.string)
@@ -190,10 +231,10 @@ def get_Time_Left(item_Time):
     source_code = requests.get(item_Time)
     plain_text = source_code.text
     soup = BeautifulSoup(plain_text)
+
     for item_Time in soup.findAll('span', {'class': 'endedDate'}):
         print('End Time:')
         print(item_Time.string)
-
 
 def get_Item_Condition(Item_condition):
     source_code = requests.get(Item_condition)
@@ -203,18 +244,17 @@ def get_Item_Condition(Item_condition):
         print('Item Condition:')
         print(Item_condition.string)
 
-def stop():
-    exit()
+
 
 main = Tk()
-
-Button(main, text='1080TI', command=nvidia1080TI).grid(row=6, column=0, sticky=W, pady=4)
-Button(main, text='1080', command=nvidia1080).grid(row=6, column=1, sticky=W, pady=4)
-Button(main, text='1070TI', command=nvidia1070TI).grid(row=6, column=2, sticky=W, pady=4)
-Button(main, text='1070', command=nvidia1070).grid(row=6, column=3, sticky=W, pady=4)
-Button(main, text='1060 6GB', command=nvidia1060_6GB).grid(row=6, column=4, sticky=W, pady=4)
-Button(main, text='RX580', command=amdRX580).grid(row=6, column=5, sticky=W, pady=4)
-#Button(main, text='stop', command=stop).grid(row=6, column=7, sticky=W, pady=4)
+Label(main, text = "Please select a GPU:").grid(row=0, column=0, sticky=W, pady=4)
+Button(main, text='1080TI', command=nvidia1080TI).grid(row=0, column=1, sticky=W, pady=4)
+Button(main, text='1080', command=nvidia1080).grid(row=0, column=2, sticky=W, pady=4)
+Button(main, text='1070TI', command=nvidia1070TI).grid(row=0, column=3, sticky=W, pady=4)
+Button(main, text='1070', command=nvidia1070).grid(row=0, column=4, sticky=W, pady=4)
+Button(main, text='1060 6GB', command=nvidia1060_6GB).grid(row=0, column=5, sticky=W, pady=4)
+Button(main, text='RX580', command=amdRX580).grid(row=0, column=6, sticky=W, pady=4)
 
 main.title('Spider GPU')
+
 mainloop()
